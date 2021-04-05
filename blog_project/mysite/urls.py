4f516113397a12624d603828b97c940aa38ffa98
subhamrajak08 <rajaksubham19@gmail.com>
2021-04-06 00:52:38 +0530

    first commit

---
+++ blog_project/mysite/urls.py
@@ -0,0 +1,47 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+
+
+#from django.contrib import admin
+#from django.urls import path
+#from blog import views
+#from django.conf.urls import url, include
+
+#urlpatterns = [
+#    path('admin/', admin.site.urls),
+#    path('', include('blog.urls')),
+#    path('accounts/login/', Loginviews .login, name='login'),
+#    path('accounts/logout/', views.logout, name='logout', kwargs={'next_page': '/'}),
+#] -->
+#from django.contrib.auth.views import LoginView, LogoutView
+
+#urlpatterns = [
+#    path('admin/', admin.site.urls),
+#    path('',include('blog.urls')),
+#    path('accounts/login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+#    path('accounts/logout/', LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
+#]
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.contrib.auth import views
+from django.contrib.auth.views import LoginView, LogoutView
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
+    url(r'^accounts/login/$', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    url(r'^accounts/logout/$', LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
+]
